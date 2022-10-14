df13961d3669d6ab3d236cb24c8bdc0064a24e98
Barry <barrycumbie@gmail.com>
2022-10-14 11:44:53 -0500

    finished C R D (no U)

--- README.md
+++ README.md
@@ -3,9 +3,40 @@
 - Video One
     - init ghyt repo w/gitignore
     - npm init -y
-    - hammer out dependencies w/ --save-dev
-    - express, nodemon, ejs, dotenv, mongo, 
-    - maybe need: body-parser, mongodb, (mongoose)
+    - hammer out dependencies <strike>w/ --save-dev</strike>
+    - express, nodemon, ejs, dotenv, mongodb
+    - maybe need: body-parser, (mongoose)
     - finishes w/ basic set up for node, express, ejs, nodemon, 
 
 - Video Two 
+  - get heroku online and runnin...troubleshoot port error, fix dependencies in pkg.json
+
+- Video Three
+  - initialize a new mongodb atlas
+  - db = chillAppz, collection = drinkz 
+  - npm i mongodb
+  - set up our main async f/n to cxn and read from db
+
+
+- Video Four 
+  - get our data from mongo to express to ejs
+  - read multiple records
+  - use .find.toArray()
+  - pass that result to EJS then step through 
+
+  - Video Five
+    - "form"ulize `<form>` for Create, Update, Delete
+    - form points to enopoints on express
+    - npm i body-parser 
+    - need that ObjectID from mongdodb 
+
+
+
+## HTML Verbs in a REST API
+
+| HTTP Verb      | Operation |
+| ----------- | ----------- |
+| GET      | Read       |
+| POST   | Create        |
+| PUT	|          Update |
+| DELETE	       |   Delete |
--- app.js
+++ app.js
@@ -1,18 +1,94 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
 const PORT = process.env.PORT || 3000;
-
-
+const herokuVar = process.env.HEROKU_NAME || "local Barry"
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const MONGO_URI = "mongodb+srv://barry:GMSk9usexg5A8p5Q@cluster0.taug6.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-
+    
 let someVar = "";
 
-app.get('/', function (req, res) {
-//   res.send('Hello World')
+async function cxnDB(){
+
+  try{
+    client.connect; 
+    const collection = client.db("chillAppz").collection("drinkz");
+    const result = await collection.find().toArray();
+      
+    // console.log("cxnDB result: ", result);
+    return result; 
+
+  }
+  catch(e){
+      console.log(e)
+  }
+  finally{
+    client.close; 
+  }
+
+
+}
+
+app.get('/', async (req, res) => {
+
+  let result = await cxnDB().catch(console.error); 
+
+  // console.log("get/: ", result);
+
   res.render('index', {
     someVar : "hello from node, express, & EJS, nodemon here too!",
-    herokuVar : process.env.HEROKU_NAME
+    herokuVar : process.env.HEROKU_NAME,  
+    drinkData : result
   })
+})
+
+app.post('/addDrink', async (req, res) => {
+
+  try {
+    // console.log("req.body: ", req.body) 
+    client.connect; 
+    const collection = client.db("chillAppz").collection("drinkz");
+    await collection.insertOne(req.body);
+      
+    res.redirect('/');
+  }
+  catch(e){
+    console.log(error)
+  }
+  finally{
+   // client.close()
+  }
+
+})
+
+app.post('/deleteDrink/:id', async (req, res) => {
+
+  try {
+    console.log("req.parms.id: ", req.params.id) 
+    
+    client.connect; 
+    const collection = client.db("chillAppz").collection("drinkz");
+    let result = await collection.findOneAndDelete( 
+      {
+        "_id": ObjectId(req.params.id)
+      }
+    )
+    .then(result => {
+      console.log(result); 
+      res.redirect('/');
+    })
+    .catch(error => console.error(error))
+  }
+  finally{
+    //client.close()
+  }
+
 })
 
 app.listen(PORT, () => {
